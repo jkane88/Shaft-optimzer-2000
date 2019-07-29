59ad0524dbd5e95cc0e20f299055e8c1221de3ba
Justin Kane <JKane@arrowacq.com>
2019-07-29 08:45:48 -0500

    complete print function and fix margin

--- src/print.js
+++ src/print.js
@@ -1,10 +1,14 @@
+
 export const print = {
     init: () => {
         $('#printBtn').on('click', function (e) {
 
             // Triggers a print preview only for the cutsTable element, with the above header.
-            $('#cutsTable, #enteredOrdersTable').printThis({
-                header: null,
+            $('.print-this').printThis({
+                importCSS: true,
+                importStyle: true,
+                loadCSS: "/assets/css/style.css",
+                header: "<h1>ORDERS TO CUT</h1>",
                 beforePrint: () => {
                     $('#enteredOrdersTable').find('th:last-child').hide();
                     $('#enteredOrdersTable').find('button').hide();
